264a2f113cf1db8f97c012cf3a8b1c3a0f6657fe
Vladimir Kondrashov <vlkondra@microsoft.com>
2016-03-20 20:56:30 -0700

    Finish adaptive markup for index.html

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 var menu_link = document.querySelector(".main-menu__toggle");
 var menu_popup = document.querySelector(".main-menu__wrapper");
-var menu_close = document.querySelector(".main-menu__close-btn");
 
 menu_link.addEventListener("click", function(event) {
+  event.preventDefault();
+
   if (menu_popup.classList.contains("main-menu__wrapper--closed")) {
     menu_popup.classList.remove("main-menu__wrapper--closed");
+    menu_link.classList.add("main-menu__toggle--opened");
   }
-});
-
-menu_close.addEventListener("click", function(event) {
-  if (!menu_popup.classList.contains("main-menu__wrapper--closed")) {
+  else {
     menu_popup.classList.add("main-menu__wrapper--closed");
+    menu_link.classList.remove("main-menu__toggle--opened");
   }
 });
