de09cee74b592269cb96737de8db37889b14fdfb
Vladimir Kondrashov <aristovlad@gmail.com>
2016-03-25 16:16:01 -0700

    Add popup and map scripts

--- js/script.js
+++ js/script.js
@@ -13,3 +13,58 @@ menu_link.addEventListener("click", function(event) {
     menu_link.classList.remove("main-menu__toggle--opened");
   }
 });
+
+var mainform = document.querySelector(".form--main");
+if (mainform)
+{
+  var first_name = mainform.querySelector("[name=first-name]");
+  var last_name = mainform.querySelector("[name=last-name]");
+  var middle_name = mainform.querySelector("[name=middle-name]");
+  var popup_ok = document.querySelector(".popup--ok");
+  var popup_error = document.querySelector(".popup--error");
+
+  mainform.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    if (!first_name.value || !last_name.value || !middle_name.value) {
+      popup_error.classList.remove("popup--closed");
+    }
+    else {
+      popup_ok.classList.remove("popup--closed");
+    }
+  });
+
+  popup_ok.querySelector(".popup__form")
+          .addEventListener("submit", function(event) {
+            event.preventDefault();
+            popup_ok.classList.add("popup--closed");
+          });
+
+  popup_error.querySelector(".popup__form")
+             .addEventListener("submit", function(event) {
+               event.preventDefault();
+               popup_error.classList.add("popup--closed");
+             });
+}
+
+if (document.querySelector("#map")) {
+  ymaps.ready(function() {
+    var myMap = new ymaps.Map("map", {
+        center: [59.938667, 30.323073],
+        zoom: 15,
+        controls: []
+      });
+
+    var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          hintContent: "Приезжайте в гости!",
+          balloonContent: "ул. Большая Конюшенная, 19/8"
+        }, {
+            iconLayout: "default#image",
+            iconImageHref: "img/icon-map-marker.svg",
+            iconImageSize: [36, 36],
+            iconImageOffset: [-18, -18]
+        });
+
+    myMap.geoObjects.add(myPlacemark);
+  });
+}
