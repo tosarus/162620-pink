71c06a0d069b03b8cca0703ab46ba7898e154928
Vladimir Kondrashov <aristovlad@gmail.com>
2016-03-27 14:18:12 -0700

    Put SVGs to sprite or html

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 var menu_link = document.querySelector(".main-menu__toggle");
 var menu_popup = document.querySelector(".main-menu__wrapper");
+var menu_icon = document.querySelector("#icon-menu-burger");
 
 menu_link.addEventListener("click", function(event) {
   event.preventDefault();
 
   if (menu_popup.classList.contains("main-menu__wrapper--closed")) {
     menu_popup.classList.remove("main-menu__wrapper--closed");
-    menu_link.classList.add("main-menu__toggle--opened");
+    var use = menu_link.querySelector("use");
+    if (use) {
+      use.setAttribute("xlink:href", "#icon-menu-cross");
+    }
   }
   else {
     menu_popup.classList.add("main-menu__wrapper--closed");
-    menu_link.classList.remove("main-menu__toggle--opened");
+    var use = menu_link.querySelector("use");
+    if (use) {
+      use.setAttribute("xlink:href", "#icon-menu-burger");
+    }
   }
 });
 
@@ -51,7 +58,7 @@ if (document.querySelector("#map")) {
   ymaps.ready(function() {
     var myMap = new ymaps.Map("map", {
         center: [59.938667, 30.323073],
-        zoom: 15,
+        zoom: 16,
         controls: []
       });
 
@@ -60,7 +67,7 @@ if (document.querySelector("#map")) {
           balloonContent: "ул. Большая Конюшенная, 19/8"
         }, {
             iconLayout: "default#image",
-            iconImageHref: "img/icon-map-marker.svg",
+            iconImageHref: "img/sprites.svg#icon-marker",
             iconImageSize: [36, 36],
             iconImageOffset: [-18, -18]
         });
