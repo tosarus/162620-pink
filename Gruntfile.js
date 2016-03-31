6f4699545ae634523a13ec742ea9f3dd92bf7918
Vladimir Kondrashov <aristovlad@gmail.com>
2016-03-30 23:21:00 -0700

    Automation

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,57 @@ module.exports = function(grunt) {
   require("load-grunt-tasks")(grunt);
 
   grunt.initConfig({
+    clean: {
+      build: ["build"]
+    },
+
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          src: ["fonts/*.{woff,woff2}", "img/*.{png,jpg}", "js/*.js"],
+          dest: "build"
+        }, {
+          expand: true,
+          flatten: true,
+          src: ["img/icons/*.svg"],
+          dest: "build/img"
+        }]
+      },
+      html: {
+        files: [{
+          expand: true,
+          src: ["*.html"],
+          dest: "build"
+        }]
+      }
+    },
+
+    csso: {
+      style: {
+        options: {
+          report: "gzip"
+        },
+        files: {
+          "build/css/style.min.css": ["build/css/style.css"]
+        }
+      }
+    },
+
+    imagemin: {
+      images: {
+        options: { optimizationLevel: 3 },
+        files: [{
+          expand: true,
+          src: ["build/img/*.{png,jpg,svg}"]
+        }]
+      }
+    },
+
     less: {
       style: {
         files: {
-          "css/style.css": "less/style.less"
+          "build/css/style.css": ["less/style.less"]
         }
       }
     },
@@ -22,23 +69,24 @@ module.exports = function(grunt) {
             "last 2 Opera versions",
             "last 2 Edge versions"
           ]})
+          ,
+          require("css-mqpacker")({
+            sort: true
+          })
         ]
       },
       style: {
-        src: "css/*.css"
+        src: "build/css/*.css"
       }
     },
 
     browserSync: {
       server: {
         bsFiles: {
-          src: [
-            "*.html",
-            "css/*.css"
-          ]
+          src: ["build/*.html", "build/css/*.css"]
         },
         options: {
-          server: ".",
+          server: "./build",
           watchTask: true,
           notify: false,
           open: true,
@@ -48,13 +96,19 @@ module.exports = function(grunt) {
     },
 
     watch: {
-      files: ["less/**/*.less"],
-      tasks: ["less", "postcss"],
-      options: {
-        spawn: false
+      html: {
+        files: ["*.html"],
+        tasks: ["copy:html"],
+        options: { spawn: false }
+      },
+      style: {
+        files: ["less/**/*.less"],
+        tasks: ["less", "postcss", "csso"],
+        options: { spawn: false }
       }
     }
   });
 
   grunt.registerTask("serve", ["browserSync", "watch"]);
+  grunt.registerTask("build", ["clean", "copy", "less", "postcss", "csso","imagemin"]);
 };
